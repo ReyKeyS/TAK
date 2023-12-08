e6cf74e7903fd4c9ddd166721227a4b7e87d50a7
ReyKeyS <104489667+ReyKeyS@users.noreply.github.com>
2023-12-08 13:19:02 +0700

    generate map

--- TAK/Form1.Designer.cs
+++ TAK/Form1.Designer.cs
@@ -28,21 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelBoard = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(419, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TAK";
+            // 
+            // panelBoard
+            // 
+            this.panelBoard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelBoard.Location = new System.Drawing.Point(175, 70);
+            this.panelBoard.Name = "panelBoard";
+            this.panelBoard.Size = new System.Drawing.Size(600, 600);
+            this.panelBoard.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Player 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(822, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Player 2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(807, 715);
+            this.ClientSize = new System.Drawing.Size(944, 681);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panelBoard);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TAK";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelBoard;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- TAK/Form1.cs
+++ TAK/Form1.cs
@@ -12,9 +12,62 @@ namespace TAK
 {
     public partial class Form1 : Form
     {
+        Button[,] map = new Button[6, 6];
+        List<Button>[,] board = new List<Button>[6, 6];
+
+        int player1_stones = 30;
+        int player2_stones = 30;
+        int player1_capstones = 1;
+        int player2_capstones = 1;
+
         public Form1()
         {
             InitializeComponent();
+
+            generateMap();
+        }
+
+        public void generateMap()
+        {
+            for (int y = 0; y < 6; y++)
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    // Map
+                    Button temp = new Button();
+                    temp.Name = y + "," + x;
+                    temp.Size = new Size(90, 90);
+                    temp.Location = new Point((x * 99)+6, (y * 99)+6);
+                    temp.BackColor = Color.White;
+                    temp.Click += btn_Click;
+
+                    map[y, x] = temp;
+                    panelBoard.Controls.Add(temp);
+
+                    // Board
+                    board[y, x] = new List<Button>();
+                }
+            }
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string[] coord = ((Button)sender).Name.ToString().Split(',');
+            int y = Convert.ToInt32(coord[0]);
+            int x = Convert.ToInt32(coord[1]);
+
+            if (board[y, x].Count < 1)
+            {
+                add_stone(y, x);
+            }
+        }
+
+        public void add_stone(int x, int y)
+        {
+            //board[y, x].Add();
+
+            map[y, x].BackColor = Color.Cyan;
+            map[y, x].Text = board[y, x].Count.ToString();
         }
     }
 }
